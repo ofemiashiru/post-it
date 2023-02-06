a46b159d83fb417eb91631799981ec096cf3f07d
System Administrator <root@Oluwafemis-Computer.local>
2023-02-06 00:43:33 +0000

    feature: tested js to see if it is connecting

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let body = document.body
+
+let heading = document.createElement('h1')
+heading.innerHTML = "Welcome to the world of Post It"
+body.appendChild(heading)
+
+let postIts = ['Add new post it', 'brand new note', 'hello there']
+
