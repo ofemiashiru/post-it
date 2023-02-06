979eb219590bc5bee15e30af2d9d8ee096e6e535
System Administrator <root@Oluwafemis-Computer.local>
2023-02-06 01:43:50 +0000

    feat: add input box to add postits

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,20 @@ let body = document.body
 
 let main = document.getElementById('main')
 
-let postIt = ['Add new post it', 'brand new note', 'hello there', 'Hello']
+let postIt = ['Add new post it', 'brand new note', 'hello there', 'Hello'];
 
-let colours = ['#ff7eb9','#ff65a3','#7afcff','#feff9c','#fff740']
+let colours = ['#ff7eb9','#ff65a3','#7afcff','#feff9c','#fff740'];
 
 
 
 for(let i = 0; i < postIt.length; i++){
     let randomNumber = Math.floor(Math.random() * 5);
-    let postItColour = colours[randomNumber]
+    let postItColour = colours[randomNumber];
+
     main.innerHTML += `
     <div style="background-color:${postItColour}" class="post-it">
         ${postIt[i]}
         <div style="background-color:${postItColour}" class="post-it-corner"></div>
     </div>
-    
     `
 }
