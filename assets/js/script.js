9463f34cb214bb755994dc4ca80e74f2edac1175
System Administrator <root@Oluwafemis-Computer.local>
2023-02-06 03:19:24 +0000

    feat: add comments to assist

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 let body = document.body
-
 let main = document.getElementById('main')
 
 let postIt = ['Organise!!', 'Every other day I organise!', 'Some of us are wise!', 'Every other person over wise!'];
-
 let colours = ['#f39a4f','#eb6092','#4ab6d9','#abcc51','#f9c847'];
-showList()
 
-function showList(){
+showList()//refreshes the post it list
+
+function showList(){//refreshes the post it list
 
     main.innerHTML = ''; //emptys the main HTML in order to refresh the list
 
@@ -35,20 +34,24 @@ function showList(){
 
 function deletePostIt(i){
     postIt.splice(i,1);
-    showList() 
+    showList() //refreshes the post it list
 }
 
 let newPostIt;
-let userInput = document.getElementById('input-box')
+let userInput = document.getElementById('input-box') //what the user inputs into the box
 
 function updateInput(){
     newPostIt = userInput.value
 }
 
-
 function addPostIt(){
-    postIt.push(newPostIt)
-    showList() 
-    newPostIt = undefined
-    userInput.value = ''
+    if(newPostIt !== undefined){ // if the newPostIt has been set, therefore is not undefined, then add new Post It!
+
+        postIt.push(newPostIt)
+        showList() //refreshes the post it list
+        newPostIt = undefined
+        userInput.value = ''
+
+    }
+    
 }
