96f710619be6d8a88c81afc2219fb8fc3b18a85f
System Administrator <root@Oluwafemis-Computer.local>
2023-02-06 01:39:01 +0000

    feat: add random colours to post its

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,19 @@ let body = document.body
 
 let main = document.getElementById('main')
 
-let postIt = ['Add new post it', 'brand new note', 'hello there']
+let postIt = ['Add new post it', 'brand new note', 'hello there', 'Hello']
 
 let colours = ['#ff7eb9','#ff65a3','#7afcff','#feff9c','#fff740']
 
+
+
 for(let i = 0; i < postIt.length; i++){
+    let randomNumber = Math.floor(Math.random() * 5);
+    let postItColour = colours[randomNumber]
     main.innerHTML += `
-    <div class="post-it">
+    <div style="background-color:${postItColour}" class="post-it">
         ${postIt[i]}
-        <div class="post-it-corner"></div>
+        <div style="background-color:${postItColour}" class="post-it-corner"></div>
     </div>
     
     `
