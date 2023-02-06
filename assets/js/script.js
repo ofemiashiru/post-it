14e128bacfb8d8513d0da51584e8062f9e9e311c
System Administrator <root@Oluwafemis-Computer.local>
2023-02-06 00:53:55 +0000

    features: started styling the postits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 let body = document.body
 
-let heading = document.createElement('h1')
-heading.innerHTML = "Welcome to the world of Post It"
-body.appendChild(heading)
+let main = document.getElementById('main')
 
 let postIts = ['Add new post it', 'brand new note', 'hello there']
 
