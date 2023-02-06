f4c3660000261c49ff3dd5120cd6ebd0fd5f273f
System Administrator <root@Oluwafemis-Computer.local>
2023-02-06 02:14:45 +0000

    feat: add delete functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,34 @@ let main = document.getElementById('main')
 
 let postIt = ['Add new post it', 'brand new note', 'hello there', 'Hello'];
 
-let colours = ['#ff7eb9','#ff65a3','#7afcff','#feff9c','#fff740'];
+let colours = ['#f39a4f','#eb6092','#4ab6d9','#abcc51','#f9c847'];
+showList()
+
+function showList(){
+
+    main.innerHTML = ''; //emptys the main HTML in order to refresh the list
+
+
+    for(let i = 0; i < postIt.length; i++){
+
+        let randomNumber = Math.floor(Math.random() * 5);
+        let postItColour = colours[randomNumber];
+
+        main.innerHTML += `
+        <div style="background-color:${postItColour}" class="post-it">
+            <i onclick="deletePostIt(${i})" class="fas fa-trash"></i>
+            <hr>
+            ${postIt[i]}
+            <div style="background-color:${postItColour}" class="post-it-corner"></div>
+        </div>
+        `
+    }
+
+}
 
 
 
-for(let i = 0; i < postIt.length; i++){
-    let randomNumber = Math.floor(Math.random() * 5);
-    let postItColour = colours[randomNumber];
-
-    main.innerHTML += `
-    <div style="background-color:${postItColour}" class="post-it">
-        <i class="fas fa-trash"></i>
-        <hr>
-        ${postIt[i]}
-        <div style="background-color:${postItColour}" class="post-it-corner"></div>
-    </div>
-
-    `
+function deletePostIt(i){
+    postIt.splice(i,1);
+    showList() 
 }
