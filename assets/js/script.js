74f09a15280ec349b8b0060bb9b82bd2b8894926
System Administrator <root@Oluwafemis-Computer.local>
2023-02-06 01:26:54 +0000

    feat: move post it divs to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,16 @@ let body = document.body
 
 let main = document.getElementById('main')
 
-let postIts = ['Add new post it', 'brand new note', 'hello there']
+let postIt = ['Add new post it', 'brand new note', 'hello there']
 
 let colours = ['#ff7eb9','#ff65a3','#7afcff','#feff9c','#fff740']
+
+for(let i = 0; i < postIt.length; i++){
+    main.innerHTML += `
+    <div class="post-it">
+        ${postIt[i]}
+        <div class="post-it-corner"></div>
+    </div>
+    
+    `
+}
