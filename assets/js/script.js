d05b0b9f5f9b792dc51a5201bffce7fdb124d00e
System Administrator <root@Oluwafemis-Computer.local>
2023-02-07 17:35:38 +0000

    refactor: add semicolons where necesarry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-let body = document.body
-let main = document.getElementById('main')
+let body = document.body;
+let main = document.getElementById('main');
 
 let postIt = [
     'I never sleep cause sleep is the cousin of death', 
@@ -10,7 +10,7 @@ let postIt = [
 
 let colours = ['orange','pink','blue','green','gold'];
 
-showList()//refreshes the post it list
+showList();//refreshes the post it list
 
 function showList(){//refreshes the post it list
 
@@ -37,10 +37,10 @@ function showList(){//refreshes the post it list
             </div>
         `
 
-        let deleteButtons = document.getElementsByClassName('delete-btn')
+        let deleteButtons = document.getElementsByClassName('delete-btn');
 
         for(let each of deleteButtons){
-            each.addEventListener('click', deletePostIt) //adding event listener to each delete button
+            each.addEventListener('click', deletePostIt); //adding event listener to each delete button
         }
     }
 
@@ -48,35 +48,35 @@ function showList(){//refreshes the post it list
 //delete function
 function deletePostIt(){
     postIt.splice(this.value,1);
-    showList() //refreshes the post it list
+    showList(); //refreshes the post it list
 }
 
 
 
 let newPostIt;
-let userInput = document.getElementById('input-box') 
+let userInput = document.getElementById('input-box'); 
 
 //update function
 function updateInput(event){
-    newPostIt = event.target.value //what the user inputs into the box
+    newPostIt = event.target.value; //what the user inputs into the box
 }
 
-userInput.addEventListener('change', updateInput) //add event listener instead of having javascript in html
+userInput.addEventListener('change', updateInput); //add event listener instead of having javascript in html
 
 
-let addBtn = document.getElementById('add-btn')
+let addBtn = document.getElementById('add-btn');
 
 //add function
 function addPostIt(){
     if(newPostIt !== undefined){ // if the newPostIt has been set, therefore is not undefined, then add new Post It!
-        postIt.push(newPostIt)
-        showList() //refreshes the post it list
-        newPostIt = undefined
-        userInput.value = ''
+        postIt.push(newPostIt);
+        showList(); //refreshes the post it list
+        newPostIt = undefined;
+        userInput.value = '';
 
     } else{
-        alert('Please enter text')
+        alert('Please enter text');
     }   
 }
 
-addBtn.addEventListener('click', addPostIt) //add event listener instead of having javascript in html
+addBtn.addEventListener('click', addPostIt); //add event listener instead of having javascript in html
